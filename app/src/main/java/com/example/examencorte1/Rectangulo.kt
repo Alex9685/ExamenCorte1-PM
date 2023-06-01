b0a3647dc9189e1f76c56b4c72b4c2893f6115ca
Tlacoyo Emplumado <alex928280@gmail.com>
2023-06-01 16:59:04 -0700

    Se realizo la clase rectangulo

--- app/src/main/java/com/example/examencorte1/Rectangulo.kt
+++ app/src/main/java/com/example/examencorte1/Rectangulo.kt
@@ -2,5 +2,19 @@ package com.example.examencorte1
 
 
 class Rectangulo {
-  
+    private var base: Int
+    private var altura: Int
+
+    constructor() {
+        base = 0
+        altura = 0
+    }
+
+    fun calcularArea(): Float {
+        return base.toFloat() * altura.toFloat()
+    }
+
+    fun calcularPagPerimetro(): Float {
+        return 2 * (base.toFloat() + altura.toFloat())
+    }
 }
