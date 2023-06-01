26190a038b1d487dfd8675ae4c943c9541505ba4
Tlacoyo Emplumado <alex928280@gmail.com>
2023-06-01 16:58:32 -0700

    Iniciando Proyecto

---
+++ app/src/main/java/com/example/examencorte1/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.examencorte1
+
+import android.app.AlertDialog
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.Button
+import android.view.View
+import android.widget.Toast
+import android.content.Intent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.content.DialogInterface
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+}
---
+++ app/src/main/java/com/example/examencorte1/Rectangulo.kt
@@ -0,0 +1,6 @@
+package com.example.examencorte1
+
+
+class Rectangulo {
+  
+}
